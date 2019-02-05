e837da9574e8bfc11e24d08a04446b2136ce72b4
Ismail <cucevicismail@gmail.com>
2019-02-05 13:59:55 +0100

    save changes

---
+++ webpack-project/webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry:{
+    bundle: './src/index.js',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
+  }
+};
